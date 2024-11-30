0142ccb8af39483a062b91e7e1b981a33db88c9a
Anurag-NURA <anuragrawatz3k@gmail.com>
2024-11-30 22:24:49 +0530

    fixed issue: server not reading .env file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
@@ -14,20 +15,23 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cookieParser());
-app.use(express.json());//use to parse json data
-app.use(express.urlencoded({ extended: true }));//use to parse form data
+//Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use('/api/users', userRoutes);
-app.use('/api/expenses', expenseRoutes);
+app.use(cookieParser());
+app.use(express.json()); //use to parse json data
+app.use(express.urlencoded({ extended: true })); //use to parse form data
+
+app.use("/api/users", userRoutes);
+app.use("/api/expenses", expenseRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")
-    ));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
+  );
 } else {
   //it means we are in development mode
   app.get("/", (req, res) => {
