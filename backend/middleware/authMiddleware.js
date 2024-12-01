aeaf5744cf019084a44a60a84fe6e1a448a5e62b
Anurag-NURA <anuragrawatz3k@gmail.com>
2024-12-01 09:31:39 +0530

    authMiddleware updated

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-import User from '../models/userModel.js';
+import User from "../models/userModel.js";
 
 //this method will protect the routes that require authentication before accessing them
 const protect = async (req, res, next) => {
@@ -11,10 +11,16 @@ const protect = async (req, res, next) => {
 
     if (token) {
       try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const secret = process.env.JWT_SECRET;
+
+        if (!secretKey) {
+          throw new Error("JWT_SECRET is not defined in environment variables");
+        }
+
+        const decoded = jwt.verify(token, secret);
         //token is decoded by passing token and the secret key as arguments in the jwt.verify function
 
-        req.user = await User.findById(decoded.userId).select('-password');
+        req.user = await User.findById(decoded.userId).select("-password");
         //request can be made from any route
         //userId because when we generated the token, we passed the userId as the payload
         //'-password' means we don't want to return the password included in the decoded object
@@ -22,15 +28,15 @@ const protect = async (req, res, next) => {
         next();
       } catch (error) {
         console.error(error);
-        res.status(401)
-        if (error.name === 'TokenExpiredError') {
-          throw new Error('Token expired, please login again');
+        res.status(401);
+        if (error.name === "TokenExpiredError") {
+          throw new Error("Token expired, please login again");
         } else {
           throw new Error("Not authorized, Invalid Token");
         }
       }
     } else {
-      res.status(401)
+      res.status(401);
       throw new Error("Not authorized, no token");
     }
   } catch (error) {
