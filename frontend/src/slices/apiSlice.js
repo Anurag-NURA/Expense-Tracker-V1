f35b602d0276846b59c66995647f6cb4f6fcd30e
Anurag-NURA <anuragrawatz3k@gmail.com>
2024-12-05 09:53:21 +0530

    finally founded right vercel.json file

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  //baseUrl: "http://localhost:5000",
+  baseUrl: "https://expense-tracker-v1-server.vercel.app/",
 });
 
 export const apiSlice = createApi({
