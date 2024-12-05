c050edcaf2feaff90db9eab906ccdacf53756171
Anurag Rawat <84304902+Anurag-NURA@users.noreply.github.com>
2024-12-05 08:22:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({origin: true}));
 //   }),
 // );
 
-// app.options('*', cors());
+ app.options('*', cors());
 
 app.use(cookieParser());
 app.use(express.json()); //use to parse json data
