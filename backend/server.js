ac010bcf111a13bfd840e4844bba3d6950a41d29
Anurag Rawat <84304902+Anurag-NURA@users.noreply.github.com>
2024-12-20 18:52:52 +0530

    Allowed origins for server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-production-domain.com'], // Add your allowed origins
+  origin: ['http://localhost:5173', 'http://localhost:3000' , process.env.ALLOWED_ORIGIN], // Add your allowed origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
